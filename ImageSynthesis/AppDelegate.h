b386694f5bfeee51cd9e1dc6d0d3ebd4f0ee55cc
我是一个TableView <18511654507@163.com>
2017-06-23 18:10:02 +0800

    Initial Commit

---
+++ ImageSynthesis/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImageSynthesis
+//
+//  Created by ZhengWei on 2017/6/23.
+//  Copyright © 2017年 ZhengWei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ImageSynthesis/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImageSynthesis
+//
+//  Created by ZhengWei on 2017/6/23.
+//  Copyright © 2017年 ZhengWei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
