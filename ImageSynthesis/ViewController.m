9e3fae1d9b724a79b4a16977f73795ddaf4fac25
我是一个TableView <18511654507@163.com>
2017-06-28 10:08:24 +0800

    commit

--- ImageSynthesis/ViewController.m
+++ ImageSynthesis/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <objc/message.h>
 
 @interface ViewController ()
 
@@ -17,8 +18,27 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    UIImage *image1 = [UIImage imageNamed:@"image.jpg"];
+    UIImage *image2 = [UIImage imageNamed:@"image.jpg"];
+    NSString *string = @"我是猪";
+    CGSize size = CGSizeMake([UIScreen mainScreen].bounds.size.width,[UIScreen mainScreen].bounds.size.height);
+    UIGraphicsBeginImageContext(size);
+    [image1 drawInRect:CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width,[UIScreen mainScreen].bounds.size.height)];
+    [image2 drawInRect:CGRectMake(0, 300, 200,200)];
+    [string drawInRect:CGRectMake(0, 200, 200, 200) withAttributes:@{NSFontAttributeName:[UIFont systemFontOfSize:20],NSForegroundColorAttributeName:[UIColor redColor]}];
+    UIImage *ZImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    UIImageView *imageV = [[UIImageView alloc]init];
+    imageV.frame = CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height);
+    imageV.image = ZImage;
+    [self.view addSubview:imageV];
+    
 }
 
+- (void)abd{
+    
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
